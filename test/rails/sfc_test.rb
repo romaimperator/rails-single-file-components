b452b8a4e3d661a675b607837360bb344b2e3813
Daniel Fox <romaimperator@gmail.com>
2017-12-12 21:17:47 -0600

    try adding a first test to test the compile engine

--- test/rails/sfc_test.rb
+++ test/rails/sfc_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
+class TestController < ActionController::Base
+end
+
 class Rails::Sfc::Test < ActiveSupport::TestCase
-  test "truth" do
-    assert_kind_of Module, Rails::Sfc
+  def setup
+    paths = ActionController::Base.view_paths
+    @assigns = { secret: "in the sauce" }
+    @view = Class.new(ActionView::Base).new(paths, @assigns)
+
+    @controller_view = TestController.new.view_context
+  end
+
+  test "a single file component can be loaded" do
+    assert_kind_of String, @view.render(template: 'test', formats: [:sfc])
   end
 end
