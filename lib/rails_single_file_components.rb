a7e9c9e54a5f68bb2adf71864513875f36e3add4
Daniel Fox <romaimperator@gmail.com>
2018-01-13 19:27:54 -0600

    update the require references

--- lib/rails_single_file_components.rb
+++ lib/rails_single_file_components.rb
@@ -3,15 +3,15 @@ $LOAD_PATH.unshift(File.expand_path('..', __FILE__))
 
 require 'sass/css'
 
-require 'transform_pipelines/base'
-require 'transform_pipelines/style'
-require 'transform_pipelines/template'
+require 'rails_single_file_components/transform_pipelines/base'
+require 'rails_single_file_components/transform_pipelines/style'
+require 'rails_single_file_components/transform_pipelines/template'
 
-require 'rails_parts/style_processor'
-require 'rails_parts/view_helpers'
-require 'rails_parts/railtie'
+require 'rails_single_file_components/rails_parts/style_processor'
+require 'rails_single_file_components/rails_parts/view_helpers'
+require 'rails_single_file_components/rails_parts/railtie'
 
-require 'data_attribute'
-require 'exceptions'
-require 'asset_compilation'
-require 'parser'
+require 'rails_single_file_components/data_attribute'
+require 'rails_single_file_components/exceptions'
+require 'rails_single_file_components/asset_compilation'
+require 'rails_single_file_components/parser'
