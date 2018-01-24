442ccf0a6803761f7e7ac88dfaa875f8876691f8
Daniel Fox <romaimperator@gmail.com>
2018-01-23 19:49:59 -0600

    remove old Sass Converter code

--- lib/rails_single_file_components/transform_pipelines/style.rb
+++ lib/rails_single_file_components/transform_pipelines/style.rb
@@ -2,38 +2,6 @@
 module RailsSingleFileComponents
   module TransformPipelines
     class Style < Base
-      class Converter < ::Sass::Tree::Visitors::Convert
-        def self.visit(root, options, format, id)
-          new(options, format, id).send(:visit, root)
-        end
-
-        def initialize(options, format, id)
-          super(options, format)
-          @id = id
-        end
-
-        def visit_rule(node)
-          rule = if node.parsed_rules
-                   [node.parsed_rules.to_s.split(',').join("#{@id},")]
-                 else
-                   node.rule
-                 end
-          if @format == :sass
-            name = selector_to_sass(rule)
-            name = "\\" + name if name[0] == ?:
-            name.gsub(/^/, tab_str) + @id + yield
-          elsif @format == :scss
-            name = selector_to_scss(rule)
-            res = name + yield
-            if node.children.last.is_a?(::Sass::Tree::CommentNode) && node.children.last.type == :silent
-              res.slice!(-3..-1)
-              res << "\n" << tab_str << "}\n"
-            end
-            res
-          end
-        end
-      end
-
       def initialize(source_io, data_attribute, convert)
         super(source_io, data_attribute)
         @convert = convert
