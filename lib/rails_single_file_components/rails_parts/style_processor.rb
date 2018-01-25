2693fc026a504fd19f4aed9781708d41db4613e7
Daniel Fox <romaimperator@gmail.com>
2018-01-24 20:15:26 -0600

    now returning language too

--- lib/rails_single_file_components/rails_parts/style_processor.rb
+++ lib/rails_single_file_components/rails_parts/style_processor.rb
@@ -3,7 +3,7 @@ module RailsSingleFileComponents
   module RailsParts
     module StyleProcessor
       def self.call(input)
-        pipeline = TransformPipelines::Style.new(input[:data], DataAttribute.compute(input[:filename]), true).transform
+        pipeline, lang = TransformPipelines::Style.new(input[:data], DataAttribute.compute(input[:filename]), true).transform
         { data: pipeline || '' }
       end
     end
